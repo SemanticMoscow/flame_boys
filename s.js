c6416a32e312723c644cc03056af5822875d871c
Alexandr <108534425+SemanticMoscow@users.noreply.github.com>
2022-07-03 15:15:21 +0300

    Add files via upload
    minor improvements

--- s.js
+++ s.js
@@ -1,9 +1,1 @@
-document.querySelectorAll('button').forEach(b=>{
-b.addEventListener('click',(e)=>{
-let r=document.createRange();
-r.selectNode(e.target);
-window.getSelection().addRange(r);
-document.execCommand('copy');
-window.getSelection().removeAllRanges();
-});
-});
+document.querySelectorAll('button').forEach(b=>{b.addEventListener('click',(e)=>{let r=document.createRange();r.selectNode(e.target);window.getSelection().addRange(r);document.execCommand('copy');window.getSelection().removeAllRanges();});});document.addEventListener('DOMContentLoaded',()=>{const a=document.querySelectorAll('a');a.forEach(l=>{let h=l.getAttribute('href');l.addEventListener('click',()=>chrome.tabs.create({active:true,url:h}));});});
