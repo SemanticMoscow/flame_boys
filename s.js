26eb8db0fb8abe40dd5cc7ad85ca7ca769e1ca7b
Alexandr <108534425+SemanticMoscow@users.noreply.github.com>
2022-07-21 09:51:09 +0300

    Add files via upload

--- s.js
+++ s.js
@@ -1,1 +1,11 @@
-document.querySelectorAll('kbd').forEach(k=>{k.addEventListener('click',(e)=>{navigator.clipboard.writeText(e.target.innerHTML);});});window.addEventListener('click',function(e){if(e.target.href!==undefined){chrome.tabs.create({url:e.target.href})}})
+document.querySelectorAll('kbd').forEach(k=>{
+    k.addEventListener('click',(e)=>{
+        navigator.clipboard.writeText(e.target.innerHTML);
+    });
+});
+window.addEventListener('click',function(e){
+    if(e.target.href!==undefined){
+        chrome.tabs.create({
+            url:e.target.href})
+        }
+    })
